26e2b830955715986b8468779129b38306443c8c
Gerardo Larios <gerardo.larios69@gmail.com>
2021-05-28 14:20:00 -0500

    08 http collect

---
+++ learnyounode/http-collect.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const url = process.argv[2]
+
+let body = ''
+
+http.get(url, response => {
+  response.on('data', data => body += data)
+  response.on('end', () => {
+    console.log(body.length)
+    console.log(body)
+  })
+})
